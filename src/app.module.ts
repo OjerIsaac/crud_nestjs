b66cc65c3129b9b3b3a0e1bbab6d4d90d00538ca
Ojer Isaac <isaacojerumu@gmail.com>
2023-01-17 01:26:14 +0100

    chores: minor cleanup in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USERNAME,
+      username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
